51e506e7afeb87b70111444cc5aaf29692cbd7e8
Peter Kroon <peterkroon06+github@gmail.com>
2018-11-07 17:33:52 +0100

    Put ISMAGS in repair_graph and canonicalize_modifications. Trim graph_utils

--- vermouth/tests/test_ptm_detection.py
+++ vermouth/tests/test_ptm_detection.py
@@ -22,6 +22,7 @@ import pytest
 
 import vermouth
 import vermouth.processors.canonicalize_modifications as canmod
+from vermouth.ismags import ISMAGS
 
 # pylint: disable=redefined-outer-name
 
@@ -185,7 +186,7 @@ def test_identify_ptms(known_ptm_graphs, atoms, edges, expected):
     molecule = make_molecule(atoms, edges)
 
     ptms = canmod.find_ptm_atoms(molecule)
-    known_ptms = [(ptm_graph, canmod.PTMGraphMatcher(molecule, ptm_graph))
+    known_ptms = [(ptm_graph, ISMAGS(molecule, ptm_graph, node_match=canmod.ptm_node_match))
                   for ptm_graph in known_ptm_graphs]
 
     found = canmod.identify_ptms(molecule, ptms, known_ptms)
